7f30d41a27996f7283fe73afd20355ae400a62f6
Sqydev <138494523+Sqydev@users.noreply.github.com>
2026-08-06 23:10:35 +0200

    WHY IS IT RANDOMLY NOT WORKING NOW

--- src/screens/hostnameSelectScreen.c
+++ src/screens/hostnameSelectScreen.c
@@ -2,6 +2,20 @@
 
 #include "../screenModules/screenModules.h"
 
+#include <stdlib.h>
+#include <string.h>
+
 void ScreenHostnameSelect(void) {
-	
+	char* tring = GetStringFromUser((Vector2i){ GetCursorPos().x, GetCursorPos().y + strlen("Hostname:              ") });	
+
+	if(!tring) { return; }
+
+	if(DATA.Hostname.string) {
+		free(DATA.Hostname.string);
+		DATA.Hostname.string = NULL;
+	}
+
+	DATA.Hostname.stringSizeOf = strlen(tring);
+	DATA.Hostname.string = tring;
+	DATA.Hostname.selected = true;
 }
